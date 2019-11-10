ad281ea169eed8f68e7b368fd2619fcf1c00a480
Zach Lowden <zach.lowden@boldcommerce.com>
2019-11-09 23:57:45 -0600

    added cors, fixed webpack env injection

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import useApolloMiddleware from './graphql';
+import cors from 'cors';
 
 const express = require('express');
 const cookieParser = require('cookie-parser');
@@ -6,10 +7,17 @@ const logger = require('morgan');
 
 const app = express();
 
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  methods: ['GET', 'OPTIONS', 'POST', 'PUT', 'HEAD']
+};
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(cors(corsOptions));
 
 app.get('/', function(req, res, next) {
   res.json({
